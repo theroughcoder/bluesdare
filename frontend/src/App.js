397a59da407a626d24505c6e57b8bdeec15b0cc1
sanchit <sanchit2ooosp1@gmail.com>
2022-04-16 11:21:57 +0530

    react-frontend-commit-1

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <a href="">Bluesdare</a>
+      </header>
+      <main>
+        <p>Product list</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
