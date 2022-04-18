4c9f8b76f142e7a6ae17579c4a5482ae6cf06760
sanchit <sanchit2ooosp1@gmail.com>
2022-04-19 02:47:07 +0530

    list products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import data from './data'
 
 function App() {
   return (
@@ -7,8 +8,24 @@ function App() {
       <header className="App-header">
           <a href="">Bluesdare</a>
       </header>
-      <main>
-        <p>Product list</p>
+      <main >
+        <p className = "featured">Featured Product</p>
+
+        <div className = "products">
+        {data.products.map((value) =>{
+          return(
+            <div className = "product" key = {value._id}>
+              <img src = {value.image} />
+              <div className = "product-info">
+                <p>{value.name}</p>
+                <p> <strong>&#x20B9; {value.price}</strong></p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          )
+        })}
+        </div>
+
       </main>
     </div>
   );
