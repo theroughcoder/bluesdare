db28e277fb2e6e3e939d7b93308b3322519d7ec6
sanchit <sanchit2ooosp1@gmail.com>
2022-06-07 19:29:32 +0530

    publish-to-heroku

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from "express";
-import data from "./data.js";
+import path from "path"
 import mongoose from "mongoose" 
 import dotenv from "dotenv"
 import seedRouter from "./router/seedRouter.js"
 import productRouter from "./router/productRouter.js"
 import userRouter from "./router/userRouter.js"
 import orderRouter from "./router/orderRouter.js"
-
+ 
  dotenv.config();
         
 mongoose.connect(process.env.MONGODB_URL).then(()=> {console.log("Connect to DB")}
@@ -20,6 +20,11 @@ app.use("/api/seed", seedRouter);
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, '/frontend/build')));
+app.get("*", (req, res) => 
+res.sendFile(path.join(_dirname, 'frontend/build/index.html')));
 
 app.use((err, req, res, next) =>{
   res.status(500).send({ message: err.message});
